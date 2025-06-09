37f8b507ba4398003d66447ac282ff9280f082a7
Prashant <prashan1837d@gmail.com>
2025-06-10 03:35:27 +0530

    Implement authentication forms; create AuthForm component, add sign-in and sign-up pages, and define validation schemas

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
+import AuthForm from '@/components/AuthForm'
+import { SignInSchema } from '@/lib/validations'
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>Sign In Form</div>
-  )
-}
+const page = () => (
+  <AuthForm
+    type='SIGN_IN'
+    schema={SignInSchema}
+    defaultValues={{
+      email: '',
+      password: ''
+    }}
+    onSubmit={() => { }}
+  />
+)
 
 export default page
