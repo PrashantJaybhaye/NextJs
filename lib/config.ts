79adfda2ae85eae94bd2d1fb10abc144b4a5d72b
Prashant <prashan1837d@gmail.com>
2025-06-14 03:21:41 +0530

    chore: update dependencies and add @upstash/ratelimit package - Added @upstash/ratelimit version 2.0.5 - Downgraded drizzle-kit from version 0.31.1 to 0.18.1

--- lib/config.ts
+++ lib/config.ts
@@ -1,13 +1,17 @@
 const config = {
-    env: {
-        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
-        imagekit: {
-            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-            privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-        },
-        databaseUrl: process.env.DATABASE_URL,
-    }
+  env: {
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    imagekit: {
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    },
+    databaseUrl: process.env.DATABASE_URL!,
+    upststack: {
+      redisUrl: process.env.UPSTASH_REDIS_URL!,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+    },
+  },
 };
 
 export default config;
